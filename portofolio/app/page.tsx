d4126e6dc3f05108699a38af283845b3a3b186bd
Gunadi <edward.stiawan28@gmail.com>
2025-11-03 13:31:41 +0700

    Final Commit

---
+++ portofolio/app/page.tsx
@@ -0,0 +1,14 @@
+// app/page.tsx
+
+import ProfileSection from './components/ProfileSection'; // Sesuaikan path
+
+export default function Home() {
+  return (
+    <main>
+      <ProfileSection />
+      {
+        
+      }
+    </main>
+  );
+}
